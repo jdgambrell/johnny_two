db7d26d7e776627d441260042a17366cbb39faf4
jerry <89285010+jdgambrell@users.noreply.github.com>
2024-06-28 23:17:10 -0400

    Corrected minor change in launch file made for testing

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -81,7 +81,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        delayed_controller_manager
-        #delayed_diff_drive_spawner,
-        #delayed_joint_broad_spawner
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
     ])
